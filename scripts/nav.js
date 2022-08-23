6cd4df9e45d40a45a7292f2d59309cc99248b0b1
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2022-08-24 00:27:02 +0530

    navbar completed without media

---
+++ scripts/nav.js
@@ -0,0 +1,281 @@
+function nav() {
+  return `
+    <div id="navbar">
+      <div id="logo"><a href="index.js"><img
+      src="https://prod.smassets.net/assets/website/2.182.0/images/logo-surveymonkey.svg"
+      alt=""
+    /></a>
+        
+      </div>
+      <div id="options">
+        <ul class="opt">
+          <li id="Products">
+            Products <i class="fa-solid fa-angle-down"></i>
+          </li>
+          <li id="Solutions">
+            Solutions <i class="fa-solid fa-angle-down"></i>
+          </li>
+          <li id="Resources">
+            Resources <i class="fa-solid fa-angle-down"></i>
+          </li>
+          <li><a href="#">Plan & Pricing</a></li>
+        </ul>
+      </div>
+      <div id="user">
+        <ul class="opt">
+          <li><a href="#">Log in</a></li>
+          <li id="signup"><a href="signup.html">Sign up free</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="menu"></div>
+    `;
+}
+function Products() {
+  return `<div class="subnav">
+  <div class="sub1">
+    <div class="subm">
+      <h5>Surveys</h5>
+    </div>
+    <div class="subm1">
+      <ul>
+        <li>
+          <a href="#"
+            ><span></span>
+            <h3>SurveyMonkey</h3>
+            <p>
+              Create & send surveys with the world’s leading online survey
+              software
+            </p>
+          </a>
+        </li>
+        <li>
+          <a href="#"
+            ><span></span>
+            <h3>Enterprise</h3>
+            <p>
+              Empower your organization with our secure survey platform
+            </p>
+          </a>
+        </li>
+        <li>
+          <a href="#"
+            ><span></span>
+            <h3>Integrati0n & Plug-ins</h3>
+            <p>Bring survey insights into your business apps</p>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="sub2">
+    <div class="subm">
+      <h5>Specialized products</h5>
+    </div>
+    <div class="subm1">
+        <ul>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>Audience</h3>
+              <p>
+                Collect survey responses from our global consumer panel
+              </p>
+            </a>
+          </li>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>CX</h3>
+              <p>Understand & improve customer experience (NPS®)</p>
+            </a>
+          </li>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>Engage</h3>
+              <p>Understand & increase employee engagement</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+    <div class="sub3">
+      <div class="subm">
+      </div>
+      <div class="subm1">
+          <ul>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>TechValidate</h3>
+                <p>Create marketing content from customer feedback</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Apply</h3>
+                <p>Collect, review & manage applications online</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Wufoo</h3>
+                <p>Gather data & payments with online forms</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>GetFeedback</h3>
+                <p>Customer feedback for Salesforce</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <a href="#" id="allproducts"
+      >View all products<i class="fa-solid fa-arrow-right"></i
+    ></a>
+  </div>
+    
+    
+  </div>`;
+}
+function Solutions() {
+  return `<div class="subnav">
+  <div class="sub1">
+    <div class="subm">
+      <h5>Survey Types</h5>
+    </div>
+    <div class="subm4">
+      <div>
+          <a href="#">Customer Satisfaction</a>
+          <a href="#">Customer Loyalty</a>
+          <a href="#">Event Surveys</a>
+      </div>
+      <div>
+          <a href="#">Employee Engagement</a>
+          <a href="#">Job Satisfaction</a>
+          <a href="#">HR Surveys</a>
+      </div>
+      <div>
+          <a href="#">Market Research</a>
+          <a href="#">Opinion Polls</a>
+          <a href="#">Concept Testing</a>
+      </div>
+      <div id="etype"><a href="#" 
+        >Explore more survey types<i class="fa-solid fa-arrow-right"></i
+      ></a></div>
+      
+    </div>
+  </div>
+  <div class="sub2">
+    <div class="subm">
+      <h5>People Powered Data for business</h5>
+    </div>
+    <div class="subm1">
+        <ul>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>Customers</h3>
+              <p>
+                  Win more business with Customer Powered Data
+              </p>
+            </a>
+          </li>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>Employees</h3>
+              <p>Build a stronger workforce with Employee Powered Data</p>                      </a>
+          </li>
+          <li>
+            <a href="#"
+              ><span></span>
+              <h3>Markets</h3>
+              <p>Validate business strategy with Market Powered Data</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+    <div class="sub3">
+      <div class="subm">
+          <h5>Solutions for teams</h5>
+      </div>
+      <div class="subm1">
+          <ul>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Customer Experience</h3>
+                <p>Delight customers & increase loyalty through feedback</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Human Resources</h3>
+                <p>Improve your employee experience, engagement & retention</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Marketing</h3>
+                <p>Create winning campaigns, boost ROI & drive growth</p>
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                ><span></span>
+                <h3>Education</h3>
+                <p>Elevate your student experience and become a data-driven institution</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+       
+  </div>
+    
+    
+  </div>
+</div>
+</div>`;
+}
+function Resources() {
+  return `<div class="subnav1">
+  <div id="subnav1-1">
+    <a href="#"
+      ><div>
+        <h3>Resources</h3>
+        <p>Best practices for using surveys & survey data</p>
+      </div>
+    </a>
+    <a href="#"
+      ><div>
+        <h3>Curiosity at Work</h3>
+        <p>Our blog about surveys, tips for business, & more</p>
+      </div>
+    </a>
+    <a href="#"
+      ><div>
+        <h3>Help Center</h3>
+        <p>Tutorials & how-to guides for using SurveyMonkey</p>
+      </div>
+    </a>
+  </div>
+<div id="subnav1-2">
+  <a href="#"
+    >Explore our 180+ survey templates
+    <i class="fa-solid fa-arrow-right"></i
+  ></a>
+</div>
+</div>`;
+}
+export { nav, Products, Solutions, Resources };
