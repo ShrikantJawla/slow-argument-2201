22968966156a9a513a7aad29423187c67fa4f945
Mr-Sharma17 <as.ashishsharma980@gmail.com>
2022-08-24 23:28:23 +0530

    added footer html files

---
+++ components/plans&pricing footer.js
@@ -0,0 +1,274 @@
+function ppFooter(){
+    return `    <hr>
+    <div id="footer">
+        <div id="top">
+            <ul id="lists">
+                <li class="labels">
+                    <a href="https://www.facebook.com/surveymonkey/">
+                        Community:
+                    </a>
+                </li>                
+                    <li>
+                        <a href="#">
+                            Our Blog
+                        </a>
+                    </li>                
+                    <li>
+                        <a href="https://www.facebook.com/surveymonkey/">
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/SurveyMonkey">
+                            Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/surveymonkey/">
+                            LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/surveymonkey/">
+                            Instagram
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/surveymonkey">
+                            YouTube
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Developers
+                        </a>
+                    </li>
+            </ul>
+            <ul id="lists">
+                <li class="labels">
+                    <a href="#">
+                        About Us:
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Leadership Team
+                    </a>
+                </li>                
+                <li>
+                    <a href="#">
+                        Board of Directors
+                    </a>
+                </li>              
+                <li>
+                    <a href="#">
+                        Investor Relations
+                    </a>
+                </li>                
+                <li>
+                    <a href="#">
+                        App Directory
+                    </a>
+                </li>                
+                <li>
+                    <a href="#">
+                        Office Locations
+                    </a>
+                </li>               
+                <li>
+                    <a href="#">
+                        Imprint
+                    </a>
+                </li>                
+                <li>
+                    <a href="#">
+                        Careers
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Sitemap
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Help
+                    </a>
+                </li>
+            </ul>
+            <ul id="lists">
+                <li class="labels">
+                    <a href="#">
+                        Policies:
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Terms of Use
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Privacy Notice
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        California Privacy Notice
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Acceptable Uses Policy
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Security Statement
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        GDPR Compliance
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Email Opt-In
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Accessibility
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Cookies Notice
+                    </a>
+                </li>                
+                <li>
+                    <a href="#">
+                        Cookies Preferences
+                    </a>
+                </li>                
+            </ul>
+        </div>
+    <hr>
+        <div id="mid">
+            <ul id="lists">
+                <li class="labels">
+                    Language:
+                </li>
+                <li>
+                    <a href="#">
+                        English
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Español
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Português
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Deutsch
+                    </a>
+                </li>
+                <li >
+                    <a href="#">
+                        Nederlands
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Français
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        Русский
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Italiano
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        Dansk
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Svenska
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        日本語
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        한국어
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        中文(繁體)
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Türkçe
+                    </a>
+                </li>
+                <li class="SL_opaque">
+                    <a href="#">
+                        Norsk
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Suomi
+                    </a>
+                </li>
+                <li>                            
+                    <a href="#">
+                        English (UK)
+                    </a>
+                </li>
+            </ul>
+        </div>
+    <hr>
+        <div id="bottom">
+            <div class="start">
+                <span>
+                    Copyright © 1999-2022 Momentive
+                </span>           
+            </div>
+            <div class="end">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.bing.com/th?id=OIP.UXif4Hs_ghTkBZe88mTWtwHaDy&w=349&h=178&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="img">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://th.bing.com/th/id/OIP.mJA6kkQhDqdEILSFA1qL8wAAAA?w=169&h=169&c=7&r=0&o=5&pid=1.7" alt="img">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+};
+export default ppFooter;
