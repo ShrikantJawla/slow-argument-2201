862b9ce9d5cf076643d9293ad2b6fdfc42c7cb1b
ShrikantJawla <shrikantjawla@gmail.com>
2022-08-26 18:03:42 +0530

    Added survey template div to import

---
+++ components/survey_templates_div.js
@@ -0,0 +1,24 @@
+/** @format */
+
+export function templates() {
+	return `<div class="container">
+				<div class="survey_template_container">
+					<div class="left_container">
+						<div class="top_search_container">
+							<h4>Showing <span id="totalCount">130</span> templates</h4>
+							<div class="search_group">
+								<input type="text" id="search_category" placeholder="Search" />
+								<i class="fa-solid fa-magnifying-glass"></i>
+							</div>
+							<h4 id="filter_h4">Filter by survey type</h4>
+						</div>
+						<div class="bottom_categories_cont">
+							<div class="categories_container">
+							</div>
+						</div>
+					</div>
+					<div class="right_container">
+					</div>
+				</div>
+			</div>`;
+}
