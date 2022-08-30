b88906a94c17222c6f6b4b6450a30f085d7f077c
Shrikant <shrikantjawla@gmail.com>
2022-08-30 18:44:34 +0530

    Added survey-tpe data array

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,21 +1,23 @@
+/** @format */
+
 let emailName = JSON.parse(localStorage.getItem("isSignedIn"));
 
 document.querySelector(".email-span").innerText = emailName.email;
 
-
-(()=> {
-    let email = emailName.email;
-    if (email.length > 17) {
-        let str = '';
-        for (let i = 0; i < 17; i++) {
-            str += email[i];
-        }
-        str += '...';
-        document.querySelector(".email-info-bigger-font").innerText = str;
-    }
+(() => {
+	let email = emailName.email;
+	if (email.length > 17) {
+		let str = "";
+		for (let i = 0; i < 17; i++) {
+			str += email[i];
+		}
+		str += "...";
+		document.querySelector(".email-info-bigger-font").innerText = str;
+	}
 })();
 
-
-
+function closeTab(event) {
+	event.target.parentNode.parentNode.remove();
+}
 
 document.querySelector(".email-info-smaller-font").innerText = emailName.email;
